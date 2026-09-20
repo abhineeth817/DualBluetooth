016eacf5ca1d527c474439423b9bcf1377173fba
abhineethanoop817 <abhineethanoop817@gmail.com>
2026-09-20 23:42:46 +0530

    Added fuctionality to dynamically add new device slots, letting you play audio from multiple audio devices at once, given you have a way to connect them phsyically. Mutiple bluetooth devices still dont work and throw an error

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -46,11 +46,16 @@ namespace WinFormsApp1
             comboBox2 = new ComboBox();
             button3 = new Button();
             button4 = new Button();
+            add_button = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(313, 255);
+            button1.Location = new Point(461, 412);
             button1.Name = "button1";
             button1.Size = new Size(150, 46);
             button1.TabIndex = 1;
@@ -60,7 +65,7 @@ namespace WinFormsApp1
             // 
             // button2
             // 
-            button2.Location = new Point(313, 316);
+            button2.Location = new Point(461, 464);
             button2.Name = "button2";
             button2.Size = new Size(150, 46);
             button2.TabIndex = 3;
@@ -81,24 +86,27 @@ namespace WinFormsApp1
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Device1", "Device2" });
-            comboBox1.Location = new Point(12, 70);
+            comboBox1.Location = new Point(195, 27);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(776, 40);
+            comboBox1.Size = new Size(770, 40);
             comboBox1.TabIndex = 5;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.SelectedIndexChanged += DynamicCombo_SelectedIndexChanged;
+
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(12, 126);
+            comboBox2.Location = new Point(195, 82);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(776, 40);
+            comboBox2.Size = new Size(770, 40);
             comboBox2.TabIndex = 6;
             comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += DynamicCombo_SelectedIndexChanged;
             // 
             // button3
             // 
-            button3.Location = new Point(111, 372);
+            button3.Location = new Point(259, 464);
             button3.Name = "button3";
             button3.Size = new Size(150, 46);
             button3.TabIndex = 7;
@@ -108,7 +116,7 @@ namespace WinFormsApp1
             // 
             // button4
             // 
-            button4.Location = new Point(518, 372);
+            button4.Location = new Point(666, 464);
             button4.Name = "button4";
             button4.Size = new Size(150, 46);
             button4.TabIndex = 8;
@@ -116,21 +124,66 @@ namespace WinFormsApp1
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
+            // add_button
+            // 
+            add_button.Location = new Point(461, 360);
+            add_button.Name = "add_button";
+            add_button.Size = new Size(150, 46);
+            add_button.TabIndex = 9;
+            add_button.Text = "New device";
+            add_button.UseVisualStyleBackColor = true;
+            add_button.Click += add_button_Click;
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.BackColor = SystemColors.ControlLight;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Location = new Point(12, 72);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1030, 216);
+            panel1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(44, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 32);
+            label2.TabIndex = 11;
+            label2.Text = "Device 1 :";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(44, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 32);
+            label3.TabIndex = 12;
+            label3.Text = "Device 2 :";
+            label3.Click += label3_Click;
+            // 
             // Form1
             // 
             AllowDrop = true;
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1054, 574);
+            Controls.Add(panel1);
+            Controls.Add(add_button);
             Controls.Add(button4);
             Controls.Add(button3);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
             Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "DualBluetooth";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -143,6 +196,9 @@ namespace WinFormsApp1
         private ComboBox comboBox2;
         private Button button3;
         private Button button4;
+
+
+        //Start of NAaudio related fuction declarations
 
         public List<string> getAudioDevices()
         {
@@ -159,18 +215,14 @@ namespace WinFormsApp1
             return devices;
         }
 
-        public List<string> getAudioDeviceIds()
+        public MMDeviceCollection getAudioDeviceIds()
         {
-            var deviceIds = new List<string>();
+
             MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
 
-            for (int i = 0; i < WaveOut.DeviceCount; i++)
-            {
-                MMDevice device = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)[i];
-                deviceIds.Add(device.ID);
-            }
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
 
-            return deviceIds;
+            return devices;
 
         }
 
@@ -183,14 +235,21 @@ namespace WinFormsApp1
             recorder = new WasapiLoopbackCapture();
             recorder.DataAvailable += RecorderOnDataAvailable;
 
-            bufferedWaveProvider = new BufferedWaveProvider(recorder.WaveFormat);
-
-            int index = comboBox2.SelectedIndex;
             MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
-            MMDevice targetDevice = enumerator.GetDevice(getAudioDeviceIds()[index]);
+            MMDevice targetDevice = (MMDevice)comboBox2.SelectedItem;
+
+            var mixFormat = targetDevice.AudioClient.MixFormat;
+            bufferedWaveProvider = new BufferedWaveProvider(mixFormat);
+
+            Console.WriteLine(targetDevice.FriendlyName);
+            Console.WriteLine(targetDevice.AudioClient.MixFormat.SampleRate);
+            Console.WriteLine(targetDevice.AudioClient.MixFormat.Channels);
+
 
             player = new WasapiOut(targetDevice, AudioClientShareMode.Shared, false, 50);
-            player.Init(bufferedWaveProvider);
+
+            SilenceProvider sp = new SilenceProvider(mixFormat);
+            player.Init(sp);
 
             player.Play();
             recorder.StartRecording();
@@ -206,5 +265,72 @@ namespace WinFormsApp1
             recorder.StopRecording();
             player.Stop();
         }
+
+        //End of NAudio related function declarations
+
+
+        // Start of dynamic comboBox related function declaration
+        private void AddDeviceComboBox()
+        {
+            // add a new combobox to the form and fill it with audio devices
+            dynamicComboCount++;
+            var devices = getAudioDeviceIds();
+            var comboBox = new ComboBox();
+            var label = new Label();
+
+            comboBox.FormattingEnabled = true;
+            comboBox.Size = new Size(comboBox2.Width, comboBox2.Height);
+
+            //seting the posittion of new comboBox below the last one
+            int padding = 15;
+            int y = comboBox2.Bottom + padding + (dynamicComboCount - 1) * (comboBox2.Height + padding);
+            comboBox.Location = new Point(comboBox2.Left, y);
+            Console.WriteLine($"Adding new comboBox at {comboBox.Location}");
+            label.Location = new Point(comboBox.Left - 150, y);
+            label.Size = new Size(150, comboBox.Height);
+            label.Text = $"Device {dynamicComboCount + 2} : ";
+
+            comboBox.Name = $"dynamicComboBox_" + dynamicComboCount;
+            comboBox.TabIndex = 100 + dynamicComboCount;
+            comboBox.SelectedIndexChanged += DynamicCombo_SelectedIndexChanged;
+
+            foreach (var d in devices)
+            {
+                comboBox.Items.Add(d);
+
+            }
+
+            //select the next device not already selected in other comboBoxes
+            if (comboBox.Items.Count > 0) comboBox.SelectedIndex = -1;
+
+            //add the new comboBox to the control of the form and the list of comboBoxes
+            panel1.Controls.Add(label);
+            panel1.Controls.Add(comboBox);
+            comboBoxes.Add(comboBox);
+        }
+
+        public void DynamicCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("DynamicCombo_SelectedIndexChanged called");
+            var comboBox = sender as ComboBox;
+            if (comboBox == null || comboBox.SelectedItem == null) return;
+
+            foreach (var c in comboBoxes)
+            {
+                if (c != comboBox && c.SelectedItem == comboBox.SelectedItem)
+                {
+                    comboBox.SelectedIndex = -1;
+                    MessageBox.Show("This device is already selected");
+                    return;
+                }
+            }
+        }
+
+        //End of dynamic comboBox related function declaration
+
+        private Button add_button;
+        private Panel panel1;
+        private Label label2;
+        private Label label3;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,13 +1,18 @@
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        //number of newly added comboBoxes
+        private int dynamicComboCount = 0;
+        private List<ComboBox> comboBoxes;
 
         public Form1()
         {
             InitializeComponent();
+            comboBoxes = new List<ComboBox> { comboBox1, comboBox2 };
         }
 
 
@@ -18,9 +23,21 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            comboBox2.Items.Clear();
-            comboBox1.Items.Clear();
-            getAudioDevices().ForEach(x => { comboBox1.Items.Add(x); comboBox2.Items.Add(x); });
+            // clears all the current comboBoxes
+            foreach (var comboBox in comboBoxes)
+            {
+                comboBox.Items.Clear();
+            }
+
+            var devices = getAudioDeviceIds();
+            for (int i = 0; i < devices.Count; i++)
+            {
+                var device = devices[i];
+                foreach (var comboBox in comboBoxes)
+                {
+                    comboBox.Items.Add(device);
+                }
+            }
         }
 
 
@@ -38,10 +55,29 @@ namespace WinFormsApp1
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            AddDeviceComboBox();
+            Console.WriteLine("Button5 clicked");
+        }
+
+        
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
